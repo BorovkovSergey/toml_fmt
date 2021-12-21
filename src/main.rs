535ccde5b2d903a72565ed44351ce6c05c4290d3
Borovkov Sergey <707grays@gmail.com>
2021-12-21 21:47:30 +0700

    added reading/sort/writing toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{
+    fs::{read_to_string, File},
+    io::Write,
+};
+
+use toml_edit::Document;
+
+fn read_to_document(path: &str) -> Document {
+    read_to_string(path)
+        .expect("Failed to read file")
+        .parse()
+        .expect("Failed to parse content")
+}
+
+fn sort_block(document: &mut Document, key: &str) {
+    if let Some(block) = document.get_mut(key) {
+        block
+            .as_table_mut()
+            .expect("failed to cast to table")
+            .sort_values();
+    }
+}
+
+fn save_to_file(document: Document, path: &str) {
+    let mut credentials_config = File::create(path).expect("Failed to create file");
+
+    credentials_config
+        .write_all(document.to_string().as_bytes())
+        .expect("Failed to write file");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = "/Users/sergeyborovkov/Developer/rust/TomlFmt/test_copy.toml";
+
+    let mut document = read_to_document(path);
+
+    sort_block(&mut document, "dependencies");
+    sort_block(&mut document, "dev-dependencies");
+
+    let path = "/Users/sergeyborovkov/Developer/rust/TomlFmt/test";
+    save_to_file(document, path);
 }
